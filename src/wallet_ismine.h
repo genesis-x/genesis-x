1c900e1095fb4c107b4bd74561bb5008a79032f9
CowsGoWuff <20851921+CowsGoWuff@users.noreply.github.com>
2020-01-08 16:49:20 -0700

    Update wallet_ismine.h

--- src/wallet_ismine.h
+++ src/wallet_ismine.h
@@ -1,6 +1,6 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
 // Copyright (c) 2009-2014 The Bitcoin developers
-// Copyright (c) 2014-2020 The GenesisX developers
+// Copyright (c) 2017-2020 The GenesisX developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
